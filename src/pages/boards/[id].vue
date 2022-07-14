24042e6847a0357a26408e45dcc46201035504fb
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-13 21:52:18 -0300

    feat: bootstrapping project

---
+++ src/pages/boards/[id].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+
+console.log(route.params);
+
+function back() {
+  router.push("/boards");
+}
+</script>
+
+<template>
+  <div>
+    Board about {{ route.params.id }}
+
+    <button @click="back()">voltar</button>
+  </div>
+</template>
