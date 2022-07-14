9e427b443f8178b919cced08c619fb31e92d0d57
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-13 23:02:15 -0300

    feat: import drawer

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import TheNavbar from "../components/TheNavbar.vue";
+import TheDrawer from "../components/TheDrawer.vue";
 </script>
 <template>
-  <TheNavbar></TheNavbar>
+  <TheNavbar />
+  <TheDrawer />
 </template>
