52473cfd2e8492053af9ea41716e901e4959bcd3
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-13 23:19:06 -0300

    feat: import dropzone in boards

--- src/pages/boards/[id].vue
+++ src/pages/boards/[id].vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
-import { useRoute, useRouter } from "vue-router";
-
-const route = useRoute();
-const router = useRouter();
-
-console.log(route.params);
-
-function back() {
-  router.push("/boards");
-}
+import AppImageDropzone from "../../components/AppImageDropzone.vue";
 </script>
 
 <template>
-  <div>
-    Board about {{ route.params.id }}
-
-    <button @click="back()">voltar</button>
-  </div>
+  <AppImageDropzone />
 </template>
