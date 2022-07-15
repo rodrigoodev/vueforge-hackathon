9e676c8444192ca3e11db087cc53abce8fdf2e2a
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-15 01:30:31 -0300

    refactor apollo in main

--- src/main.ts
+++ src/main.ts
@@ -4,40 +4,8 @@ import App from "./App.vue";
 import router from "./router";
 import "@/assets/base.css";
 import "@progress/kendo-theme-default/dist/all.css";
-import {
-  ApolloClient,
-  createHttpLink,
-  gql,
-  InMemoryCache,
-} from "@apollo/client/core";
 import { DefaultApolloClient } from "@vue/apollo-composable";
-
-const httpLink = createHttpLink({
-  uri: "https://api.8base.com/cl5bir51u00lb09id1nfmfozv",
-});
-
-const cache = new InMemoryCache();
-
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-});
-
-// apolloClient.query({
-//   query: gql(`
-//     query BoardsList{
-//       boardsList {
-//         items {
-//           id
-//           title
-//           image {
-//             downloadUrl
-//           }
-//         }
-//       }
-//     }
-//   `),
-// });
+import { apolloClient } from "@/graphql/apolloClient";
 
 const app = createApp({
   setup() {
@@ -47,7 +15,6 @@ const app = createApp({
   render: () => h(App),
 });
 
-app.use(router);
 app.use(router).use(createPinia());
 
 app.mount("#app");
