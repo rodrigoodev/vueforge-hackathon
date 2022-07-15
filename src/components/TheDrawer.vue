0450fcad21213d6432a26e756f81ef9f8a8a497b
Rodrigo Barros <rodrigorgrb@gmail.com>
2022-07-15 01:54:13 -0300

    fix draw select

--- src/components/TheDrawer.vue
+++ src/components/TheDrawer.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { Drawer, DrawerContent } from "@progress/kendo-vue-layout";
-import { useRouter } from "vue-router";
-import { computed, ref } from "vue";
 import { useLocalStorage } from "@vueuse/core";
+import { useRouter } from "vue-router";
+
+import { computed, ref } from "vue";
 
 const router = useRouter();
 const selectedId = ref(0);
@@ -11,41 +12,45 @@ const expanded = useLocalStorage("vue-forge-drawer-expanded", true);
 const expandedIcon = computed(() =>
   expanded.value ? "k-i-arrow-chevron-left" : "k-i-arrow-chevron-right"
 );
-
-const items = computed(() => [
-  {
-    text: "Boards",
-    icon: "k-i-set-column-position",
-    selected: true,
-    data: {
-      path: "/",
+const items = computed(() =>
+  [
+    {
+      text: "Boards",
+      icon: "k-i-set-column-position",
+      data: {
+        path: "/",
+      },
     },
-  },
-  {
-    text: "Templates",
-    icon: "k-i-border-left",
-    data: {
-      path: "/templates",
+    {
+      text: "Templates",
+      icon: "k-i-border-left",
+      data: {
+        path: "/templates",
+      },
     },
-  },
-  {
-    text: "Settings",
-    icon: "k-i-gear",
-    data: {
-      path: "/settings",
+    {
+      text: "Settings",
+      icon: "k-i-gear",
+      data: {
+        path: "/settings",
+      },
     },
-  },
-  {
-    text: "Collapse",
-    icon: expandedIcon.value,
-    data: {
-      action: () => (expanded.value = !expanded.value),
+    {
+      text: "Collapse",
+      icon: expandedIcon.value,
+      data: {
+        action: () => (expanded.value = !expanded.value),
+      },
     },
-  },
-]);
+  ].map((item, index) => ({
+    ...item,
+    selected: index === selectedId.value,
+  }))
+);
 
 function onSelect({ itemIndex }: { itemIndex: number }) {
   const item = items.value[itemIndex];
+  selectedId.value = itemIndex;
   if (item.data.path) router.push(item.data.path);
   if (typeof item.data.action === "function") item.data.action();
 }
@@ -62,7 +67,7 @@ function onSelect({ itemIndex }: { itemIndex: number }) {
     @select="onSelect"
   >
     <DrawerContent>
-      <div class="p-5">
+      <div class="px-5">
         <router-view />
       </div>
     </DrawerContent>
